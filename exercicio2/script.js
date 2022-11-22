0c6d5c0fddc8d8fadc74424761da3f45fe224c1e
luiznijah <luiznijah@gmail.com>
2022-11-22 19:20:34 -0300

    resolução dos exercicios

---
+++ exercicio2/script.js
@@ -0,0 +1,11 @@
+const numA = Number (prompt ("digite um numero aleatorio"))
+const numB = Number (prompt ("digite um numero aleatorio"))
+
+console.log ("O primeiro numero ",numA," é maior que segundo?",numB ,numA > numB);
+
+console.log ("O primeiro numero ",numA," é igual ao segundo?",numB , numA === numB);
+
+console.log ( "O primeiro numero ",numA," é divisível pelo segundo?",numB ,numA % numB === 0)
+
+console.log ("O segundo numero ",numB ," é divisível pelo primeiro ",numA,"?", numB % numA === 0)
+
